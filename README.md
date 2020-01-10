303d21dd467fad8f869bb684a72a8b52c0f614fd
Eric Wong <eric@curology.com>
2020-01-10 10:40:23 -0800

    Add email replacement from arguments

---
+++ README.md
@@ -0,0 +1,8 @@
+# Carpet  
+A soft, fuzzy layer you throw over Parquet.
+
+This program takes date and comma separated list of email addresses to destroy
+ex:
+```
+carpet 2020-1-30 foo@bar.com,baz@qux.org
+```
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use std::env;
 use std::path::Path;
 
 mod parquet_file;
 use parquet_file::ParquetFile;
 
 fn main() {
-    let email_to_remove = String::from("bkassidy98@gmail.com");
+    let args: Vec<String> = env::args().collect();
+    let email_to_remove = args[1].clone();
     let email_to_replace = String::from("ghost@curology.com");
 
     let file_paths = vec![Path::new("***REMOVED***")];
@@ -21,26 +23,4 @@ fn main() {
         ))
         .unwrap();
     }
-
-    // let wrote_file = File::open(&Path::new(
-    //     "***REMOVED***",
-    // ))
-    // .unwrap();
-    // let wrote_reader = SerializedFileReader::new(wrote_file).unwrap();
-    // let mut iter = wrote_reader.get_row_iter(None).unwrap();
-    // let meta = wrote_reader.metadata();
-    // let rgcount = meta.num_row_groups();
-    // let nr = meta.row_group(0).num_rows() as usize;
-
-    // println!("Row Groups: {}", rgcount);
-    // println!("Rows: {}", nr);
-    // // println!("Columns: {}", row_group_metadata.num_columns());
-    // let mut i = 200;
-    // while let Some(record) = iter.next() {
-    //     println!("entries: {}", record);
-    //     i -= 1;
-    //     if i == 0 {
-    //         break;
-    //     }
-    // }
 }
