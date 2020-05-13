637c8d9a7c4093ff61c763e0fe3e76305b3ee7f7
Eric Wong <K4orta@users.noreply.github.com>
2020-05-13 15:19:46 -0700

    Add additional column types and DRY things up with Macros (#11)

--- src/carpet_parquet/column.rs
+++ src/carpet_parquet/column.rs
@@ -1,12 +1,19 @@
 use parquet::column::reader::ColumnReader;
 use parquet::column::writer::ColumnWriter;
 use parquet::data_type::ByteArray;
+use parquet::data_type::Int96;
 use parquet::memory::BufferPtr;
 use std::io::Error;
 
+#[derive(Default)]
 pub struct ParquetColumn {
     int_values: Vec<i32>,
+    int64_values: Vec<i64>,
+    int96_values: Vec<Int96>,
     byte_values: Vec<ByteArray>,
+    float_values: Vec<f32>,
+    double_values: Vec<f64>,
+    bool_values: Vec<bool>,
     def_levels: Vec<i16>,
     rep_levels: Vec<i16>,
     pub is_dirty: bool,
@@ -16,6 +23,48 @@ fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
     haystack
         .windows(needle.len())
         .position(|window| window == needle)
+}
+
+macro_rules! gen_converter {
+    ($name:ident, $field:ident, $native_type:ty) => {
+        fn $name(values: Vec<$native_type>, def_levels: Vec<i16>, rep_levels: Vec<i16>) -> Self {
+            Self {
+                $field: values,
+                def_levels,
+                rep_levels,
+                is_dirty: false,
+                ..Default::default()
+            }
+        }
+    };
+}
+
+macro_rules! gen_reader {
+    ($i:ident, $tr:ident, $bs:ident, $def:ident, $rep:ident) => {
+        let mut values = vec![Default::default(); $bs];
+        while let Ok((read, _)) = $tr.read_batch(
+            $bs, // batch size
+            Some(&mut $def),
+            Some(&mut $rep),
+            &mut values,
+        ) {
+            if read < $bs {
+                return Ok(ParquetColumn::$i(values, $def, $rep));
+            }
+        }
+    };
+}
+
+macro_rules! gen_writer {
+    ($self:ident, $writer:ident, $values:ident) => {
+        $writer
+            .write_batch(
+                &$self.$values,
+                Some($self.def_levels.as_slice()),
+                Some($self.rep_levels.as_slice()),
+            )
+            .unwrap();
+    };
 }
 
 impl ParquetColumn {
@@ -30,53 +79,44 @@ impl ParquetColumn {
         let mut column_reader = row_group_reader.get_column_reader(column_index).unwrap();
         match column_reader {
             ColumnReader::ByteArrayColumnReader(ref mut typed_reader) => {
-                let mut values = vec![Default::default(); num_values];
-                while let Ok((read, _)) = typed_reader.read_batch(
-                    batch_size, // batch size
-                    Some(&mut def_levels),
-                    Some(&mut rep_levels),
-                    &mut values,
-                ) {
-                    if read < batch_size {
-                        return Ok(ParquetColumn::from_bytes(values, def_levels, rep_levels));
-                    }
-                }
+                gen_reader!(from_bytes, typed_reader, batch_size, def_levels, rep_levels);
             }
             ColumnReader::Int32ColumnReader(ref mut typed_reader) => {
-                let mut values = vec![Default::default(); num_values];
-                while let Ok((read, _)) = typed_reader.read_batch(
-                    batch_size, // batch size
-                    Some(&mut def_levels),
-                    Some(&mut rep_levels),
-                    &mut values,
-                ) {
-                    if read < batch_size {
-                        return Ok(ParquetColumn::from_int(values, def_levels, rep_levels));
-                    }
-                }
+                gen_reader!(from_int32, typed_reader, batch_size, def_levels, rep_levels);
+            }
+            ColumnReader::Int96ColumnReader(ref mut typed_reader) => {
+                gen_reader!(from_int96, typed_reader, batch_size, def_levels, rep_levels);
+            }
+            ColumnReader::BoolColumnReader(ref mut typed_reader) => {
+                gen_reader!(from_bool, typed_reader, batch_size, def_levels, rep_levels);
+            }
+            ColumnReader::FloatColumnReader(ref mut typed_reader) => {
+                gen_reader!(from_float, typed_reader, batch_size, def_levels, rep_levels);
+            }
+            ColumnReader::DoubleColumnReader(ref mut typed_reader) => {
+                gen_reader!(
+                    from_double,
+                    typed_reader,
+                    batch_size,
+                    def_levels,
+                    rep_levels
+                );
+            }
+            ColumnReader::Int64ColumnReader(ref mut typed_reader) => {
+                gen_reader!(from_int64, typed_reader, batch_size, def_levels, rep_levels);
             }
             _ => unimplemented!(),
         }
         Ok(ParquetColumn::from_bytes(vec![], def_levels, rep_levels))
     }
-    fn from_bytes(values: Vec<ByteArray>, def_levels: Vec<i16>, rep_levels: Vec<i16>) -> Self {
-        Self {
-            int_values: vec![],
-            byte_values: values,
-            def_levels,
-            rep_levels,
-            is_dirty: false,
-        }
-    }
-    fn from_int(values: Vec<i32>, def_levels: Vec<i16>, rep_levels: Vec<i16>) -> Self {
-        Self {
-            int_values: values,
-            byte_values: vec![],
-            def_levels,
-            rep_levels,
-            is_dirty: false,
-        }
-    }
+
+    gen_converter!(from_bool, bool_values, bool);
+    gen_converter!(from_bytes, byte_values, ByteArray);
+    gen_converter!(from_int32, int_values, i32);
+    gen_converter!(from_int64, int64_values, i64);
+    gen_converter!(from_int96, int96_values, Int96);
+    gen_converter!(from_float, float_values, f32);
+    gen_converter!(from_double, double_values, f64);
 
     pub fn write(
         &self,
@@ -85,22 +125,25 @@ impl ParquetColumn {
         if let Some(mut col_writer) = writer.next_column().unwrap() {
             match col_writer {
                 ColumnWriter::ByteArrayColumnWriter(ref mut typed_writer) => {
-                    typed_writer
-                        .write_batch(
-                            &self.byte_values,
-                            Some(self.def_levels.as_slice()),
-                            Some(self.rep_levels.as_slice()),
-                        )
-                        .unwrap();
+                    gen_writer!(self, typed_writer, byte_values);
                 }
                 ColumnWriter::Int32ColumnWriter(ref mut typed_writer) => {
-                    typed_writer
-                        .write_batch(
-                            &self.int_values,
-                            Some(self.def_levels.as_slice()),
-                            Some(self.rep_levels.as_slice()),
-                        )
-                        .unwrap();
+                    gen_writer!(self, typed_writer, int_values);
+                }
+                ColumnWriter::Int64ColumnWriter(ref mut typed_writer) => {
+                    gen_writer!(self, typed_writer, int64_values);
+                }
+                ColumnWriter::Int96ColumnWriter(ref mut typed_writer) => {
+                    gen_writer!(self, typed_writer, int96_values);
+                }
+                ColumnWriter::BoolColumnWriter(ref mut typed_writer) => {
+                    gen_writer!(self, typed_writer, bool_values);
+                }
+                ColumnWriter::FloatColumnWriter(ref mut typed_writer) => {
+                    gen_writer!(self, typed_writer, float_values);
+                }
+                ColumnWriter::DoubleColumnWriter(ref mut typed_writer) => {
+                    gen_writer!(self, typed_writer, double_values);
                 }
                 _ => {}
             }
